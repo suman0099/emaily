6c46695663578e88eddbbb7613b80ec160f76131
Suman Mahato <sumanmahato0099@gmail.com>
2020-02-10 21:01:32 +0530

    SurveyList bug fix

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -19,6 +19,8 @@ router.get('/api/surveys', async (req, res) => {
     const surveys = await Survey.find({ _user: req.user.id }).select({
         recipients: false
     });
+
+    res.send(surveys);
 });
 
 router.post('/api/surveys/webhooks', (req, res) => {
