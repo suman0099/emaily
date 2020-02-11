657fd1be5351f6e48a0429c7d7e37b4454ac6a73
Suman Mahato <sumanmahato0099@gmail.com>
2020-02-11 14:30:17 +0530

    Survey List render bug

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -8,14 +8,14 @@ class SurveyList extends Component {
     }
 
     renderSurveys() {
-        this.props.surveys.reverse().map(survey => {
+        return this.props.surveys.reverse().map(survey => {
             return (
                 <div className="card darken-1" key={survey._id}>
                     <div className="card-content">
                         <span className="card-title">{survey.title}</span>
                         <p>{survey.body}</p>
                         <p className="right">
-                            Sent On:
+                            Sent On:{' '}
                             {new Date(survey.dateSent).toLocaleDateString()}
                         </p>
                     </div>
@@ -34,9 +34,7 @@ class SurveyList extends Component {
 }
 
 function mapStateToProps({ surveys }) {
-    return {
-        surveys
-    };
+    return { surveys };
 }
 
 export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
